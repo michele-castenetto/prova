b3255565f8785968a42eed7071b4ffab75383fea
michele.castenetto <michele.castenetto@tesysoftware.com>
2017-02-10 15:30:05 +0100

    added index.html app.js app.css

---
+++ app.js
@@ -0,0 +1,9 @@
+(function() {
+
+    var _$app = $('#app');
+
+    var template = '<h1> {{title}} </h1>';
+    var html = template.replace(new RegExp('{{title}}', 'g'), "APP");
+    _$app.html(html);
+    
+})();
