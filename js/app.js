d22b8804cfd214df0e64f8dc657f03e8f24cd60a
michele.castenetto <michele.castenetto@tesysoftware.com>
2017-02-10 16:45:44 +0100

    add simple list creation to app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
 
-
-
-
 (function() {
 
     var _$app = $('#app');
@@ -9,7 +6,16 @@
     var template = '<h1> {{title}} </h1>';
     var html = template.replace(new RegExp('{{title}}', 'g'), "APP");
     _$app.html(html);
-    
+
+    _$app.append('<div class="list"><div>');
+
+    toAppend = "";
+
+    ["uno", "due", "tre"].forEach(function(item) {
+        toAppend += '<div>'+ item +'</div>';   
+    });
+
+    _$app.find('.list').append(toAppend);
 
 })();
 
